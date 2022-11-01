21e487adf07a28db4438424d1d1f494773c06a57
Gabriel Fior <fior.gbm@gmail.com>
2022-11-01 20:19:09 +0100

    Fixed Foundry test

--- hardhat/test/foundry/VolcanoCoin_hw9.t.sol
+++ hardhat/test/foundry/VolcanoCoin_hw9.t.sol
@@ -3,37 +3,42 @@ pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
 import "../../contracts/VolcanoNFT_hw9.sol";
+import "forge-std/console.sol";
 
 contract VolcanoNFT_hw9Test is Test {
     
     VolcanoNFTHw9 private volcanoNFT;
-    address deployer;
+    address owner;
     address otherAccount;
+    address ZERO_ADDRESS = address(0);
 
     function setUp() public {
         volcanoNFT = new VolcanoNFTHw9("NFT1","myNFT");
-        deployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+        owner = address(this);
         otherAccount = vm.addr(2);
     }
 
-    event Transfer(address from, address to, uint256 tokenId);
+    //event Transfer(address from, address to, uint256 tokenId);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address owner, address approved, uint256 tokenId);
 
-    function testMintEmitsEvent() public {
-        vm.expectEmit(true, true, false, true);
-        emit Transfer(0x0000000000000000000000000000000000000000, deployer, 1);
-        volcanoNFT.mint(deployer, "abc");
+    function testMintEmitsEvent() public {        
+        vm.expectEmit(true, true, true, true);
+        emit Transfer(ZERO_ADDRESS, owner, 1);
+        volcanoNFT.mint(owner, "abc");
     }
 
     function testMint() public {
-        volcanoNFT.mint(deployer, "abc");
-        uint256 balance = volcanoNFT.balanceOf(deployer);
+        volcanoNFT.mint(owner, "abc");
+        uint256 balance = volcanoNFT.balanceOf(owner);
         assertEq(balance, 1);
     }
 
+
      function testTransfer() public {
-        volcanoNFT.mint(deployer, "abc");
-        volcanoNFT.transferFrom(deployer, otherAccount, 1);
-        assertEq(volcanoNFT.balanceOf(deployer), 0);
+        volcanoNFT.mint(owner, "abc");
+        volcanoNFT.transferFrom(owner, otherAccount, 1);
+        assertEq(volcanoNFT.balanceOf(owner), 0);
         assertEq(volcanoNFT.balanceOf(otherAccount), 1);
     }
 }
